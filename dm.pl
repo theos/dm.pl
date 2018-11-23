b7665aff0ee515b7bbb6da9491f35b0e28795b96
uroboro <uroboro845@gmail.com>
2018-11-23 11:30:15 -0300

    Map compression level 0 to 1 when using gzip.
    Fixes #5.

--- dm.pl
+++ dm.pl
@@ -53,7 +53,7 @@ if($compresslevel < 0 || $compresslevel > 9) {
 	$compresslevel = 9 if $compression eq "bzip2";
 }
 
-if($compression eq "bzip2" && $compresslevel eq 0) {
+if(($compression eq "bzip2" || $compression eq "gzip") && $compresslevel eq 0) {
 	$compresslevel = 1;
 }
 
@@ -237,7 +237,7 @@ Specify the package compression type. Valid values are gzip (default), bzip2, lz
 
 =item B<-zE<lt>compress-levelE<gt>>
 
-Specify the package compression level. Valid values are between 0 and 9. Default is 9 for bzip2, 6 for others. 0 is identical to 1 when using bzip2. Refer to B<gzip(1)>, B<bzip2(1)>, B<xz(1)> for explanations of what effect each compression level has.
+Specify the package compression level. Valid values are between 0 and 9. Default is 9 for bzip2, 6 for others. 0 is identical to 1 when using bzip2 and gzip. Refer to B<gzip(1)>, B<bzip2(1)>, B<xz(1)> for explanations of what effect each compression level has.
 
 =item B<--help>, B<-?>
 
